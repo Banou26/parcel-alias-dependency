c00ecc042d6210b2cc7e63f6f5bbb5d635cef188
Banou26 <me@banou.dev>
2020-01-08 16:08:02 +0100

    feat: implement more advanced tests

--- packages/foo/index.js
+++ packages/foo/index.js
@@ -1,3 +1,8 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+console.log('foo', React)
 
 export default React
+
+export const hook = () => useSelector(state => state.test)
--- packages/main/index.js
+++ packages/main/index.js
@@ -1,4 +1,27 @@
 import React from 'react'
-import FooReact from 'foo'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import FooReact, { hook } from 'foo'
 
+console.log('main', React)
 console.log(FooReact === React)
+
+const store = createStore(state => state, {})
+
+const App = () => {
+  const hookResult = hook()
+  console.log(hookResult)
+  return (
+    <>
+      test
+    </>
+  )
+}
+
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.body.appendChild(document.createElement('div'))
+)
