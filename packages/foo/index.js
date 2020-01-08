dc2839ae51eed4a4f326fb6e85505a97e33080e3
Banou26 <me@banou.dev>
2020-01-08 16:40:26 +0100

    feat: display how the bug work
    if yarn install root & yarn dev, instance is shared
    
    if yarn install in any of the sub packages & yarn dev at root, instance isn't shared

--- packages/foo/index.js
+++ packages/foo/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 
 console.log('foo', React)
 
 export default React
-
-export const hook = () => useSelector(state => state.test)
--- packages/main/index.js
+++ packages/main/index.js
@@ -1,27 +1,5 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import FooReact, { hook } from 'foo'
+import FooReact from 'foo'
 
 console.log('main', React)
 console.log(FooReact === React)
-
-const store = createStore(state => state, {})
-
-const App = () => {
-  const hookResult = hook()
-  console.log(hookResult)
-  return (
-    <>
-      test
-    </>
-  )
-}
-
-render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
-  document.body.appendChild(document.createElement('div'))
-)
